94ccec24951638ebab89ba51c741b87ec9de3ad2
Frameck <fra9879@gmail.com>
2022-01-12 16:24:03 +0100

    Created model, resource controller and migration

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+use Illuminate\Support\Facades\DB;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // foreach ($comics as $comic) {
+        //     DB::table('comics')->insert($comic);
+        // }
+    }
+}
